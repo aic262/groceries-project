81b63ba08f69ea1e322adb1eb6f794995b4032eb
aic262 <aic262@stern.nyu.edu>
2017-07-12 19:11:47 -0400

    submit groceries project

---
+++ groceries.py
@@ -0,0 +1,79 @@
+import code
+import operator
+from collections import Counter
+
+products = [
+    {"id":1, "name": "Chocolate Sandwich Cookies", "department": "snacks", "aisle": "cookies cakes", "price": 3.50},
+    {"id":2, "name": "All-Seasons Salt", "department": "pantry", "aisle": "spices seasonings", "price": 4.99},
+    {"id":3, "name": "Robust Golden Unsweetened Oolong Tea", "department": "beverages", "aisle": "tea", "price": 2.49},
+    {"id":4, "name": "Smart Ones Classic Favorites Mini Rigatoni With Vodka Cream Sauce", "department": "frozen", "aisle": "frozen meals", "price": 6.99},
+    {"id":5, "name": "Green Chile Anytime Sauce", "department": "pantry", "aisle": "marinades meat preparation", "price": 7.99},
+    {"id":6, "name": "Dry Nose Oil", "department": "personal care", "aisle": "cold flu allergy", "price": 21.99},
+    {"id":7, "name": "Pure Coconut Water With Orange", "department": "beverages", "aisle": "juice nectars", "price": 3.50},
+    {"id":8, "name": "Cut Russet Potatoes Steam N' Mash", "department": "frozen", "aisle": "frozen produce", "price": 4.25},
+    {"id":9, "name": "Light Strawberry Blueberry Yogurt", "department": "dairy eggs", "aisle": "yogurt", "price": 6.50},
+    {"id":10, "name": "Sparkling Orange Juice & Prickly Pear Beverage", "department": "beverages", "aisle": "water seltzer sparkling water", "price": 2.99},
+    {"id":11, "name": "Peach Mango Juice", "department": "beverages", "aisle": "refrigerated", "price": 1.99},
+    {"id":12, "name": "Chocolate Fudge Layer Cake", "department": "frozen", "aisle": "frozen dessert", "price": 18.50},
+    {"id":13, "name": "Saline Nasal Mist", "department": "personal care", "aisle": "cold flu allergy", "price": 16.00},
+    {"id":14, "name": "Fresh Scent Dishwasher Cleaner", "department": "household", "aisle": "dish detergents", "price": 4.99},
+    {"id":15, "name": "Overnight Diapers Size 6", "department": "babies", "aisle": "diapers wipes", "price": 25.50},
+    {"id":16, "name": "Mint Chocolate Flavored Syrup", "department": "snacks", "aisle": "ice cream toppings", "price": 4.50},
+    {"id":17, "name": "Rendered Duck Fat", "department": "meat seafood", "aisle": "poultry counter", "price": 9.99},
+    {"id":18, "name": "Pizza for One Suprema Frozen Pizza", "department": "frozen", "aisle": "frozen pizza", "price": 12.50},
+    {"id":19, "name": "Gluten Free Quinoa Three Cheese & Mushroom Blend", "department": "dry goods pasta", "aisle": "grains rice dried goods", "price": 3.99},
+    {"id":20, "name": "Pomegranate Cranberry & Aloe Vera Enrich Drink", "department": "beverages", "aisle": "juice nectars", "price": 4.25}
+] #<-- Copy and paste the provided products list here (see above!)
+
+#
+# products
+#
+
+print(type(products))
+print(len(products))
+
+def sort_by_team_name(product):
+    return product ["name"]
+
+products = sorted(products, key=operator.itemgetter("name"))
+
+print("THERE ARE",len(products),"PRODUCTS:")
+
+for product in products:
+    price_usd = '(${0:.2f})'.format(product["price"])
+    print(' +',product["name"].format(product["name"]), price_usd)
+
+
+print("-----------------------")
+
+#
+# departments
+#
+
+
+department_name=[]
+
+for product in products:
+    department_name.append(product["department"])
+
+department_name = list(set(department_name))
+department_name.sort()
+
+def count_depts(department_name):
+    return [prodcut for product in products if product["department"] == department_name]
+
+def get_products(department_name):
+    return [product for product in products if product["department"] == department_name]
+
+print ("THERE ARE",len(department_name),"DEPARTMENTS:")
+
+for dep in department_name:
+   matching_products= get_products(dep)
+   print ("+",dep.title(),"(" + str(len(matching_products)),"Products)")
+
+
+
+
+
+
+#code.interact(local=locals()) #debugging our scripts or dropping a break point -- use exit() to exit
